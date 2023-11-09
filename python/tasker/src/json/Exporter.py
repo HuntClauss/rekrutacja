011e086f6fb992ab21e7cf5154beb0d62d00c9e0
HuntClauss <hunt.clauss@proton.me>
2023-11-09 15:27:38 +0100

    feat(tasker-json): implement Importer & Exporter

--- python/tasker/src/json/Exporter.py
+++ python/tasker/src/json/Exporter.py
@@ -6,6 +6,6 @@ class Exporter:
     def __init__(self):
         pass
 
-    def save_tasks(self, tasks):
-        # TODO zapisz taski do pliku tutaj
-        pass
+    def save_tasks(self, tasks: list[dict]) -> None:
+        with open("./taski.json", "w", encoding="utf8") as f:
+            json.dump(tasks, f, ensure_ascii=False)
--- python/tasker/src/json/Importer.py
+++ python/tasker/src/json/Importer.py
@@ -2,14 +2,15 @@ import json
 
 
 class Importer:
+    __slots__ = ["tasks"]
 
     def __init__(self):
+        self.tasks: list[dict] = []
         pass
 
-    def read_tasks(self):
-        # TODO odczytaj plik i zdekoduj treść tutaj
-        pass
+    def read_tasks(self) -> None:
+        with open("./taski.json", "r", encoding="utf8") as f:
+            self.tasks = json.load(f)
 
-    def get_tasks(self):
-        # TODO zwróć zdekodowane taski tutaj
-        pass
+    def get_tasks(self) -> list[dict]:
+        return self.tasks
